e002c692a038515e94ddc20ac3ca0311d6e679c8
Fernando <cbm0100687arriaga@gmail.com>
2025-01-28 22:26:35 -0600

    Fix GitHub Actions deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://fernandoarriaga87.github.io/astromipag.github.io/",
+  base: "/astromipag.github.io",
   integrations: [tailwind()]
 });
